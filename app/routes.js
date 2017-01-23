547a3fae39659c83c47289eb999bc9ef0060f464
Nick Staggs <nick.c.staggs@gmail.com>
2017-01-22 20:27:10 -0500

    Started work on routing in server.js

--- app/routes.js
+++ app/routes.js
@@ -1,9 +1,23 @@
+var Nerd = require('./models/Nerd');
+
+
 module.exports = function(app) {
 
 	// server routes ===========================================================
 	// handle things like api calls
 	// authentication routes
+	app.get('/api/nerds', function(req, res) {
+            // use mongoose to get all nerds in the database
+      Nerd.find(function(err, nerds) {
 
+          // if there is an error retrieving, send the error.
+                          // nothing after res.send(err) will execute
+          if (err)
+              res.send(err);
+
+          res.json(nerds); // return all nerds in JSON format
+      });
+  });
 	// frontend routes =========================================================
 	// route to handle all angular requests
 	app.get('*', function(req, res) {
