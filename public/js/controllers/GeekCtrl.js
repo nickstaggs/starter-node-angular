9616b893f954e79fb5101c898974f1f5ffc90663
Nick Staggs <nick.c.staggs@gmail.com>
2017-01-26 20:14:24 -0500

    Started db piece

--- public/js/controllers/GeekCtrl.js
+++ public/js/controllers/GeekCtrl.js
@@ -1,5 +1,11 @@
 angular.module('GeekCtrl', []).controller('GeekController', function($scope) {
 
 	$scope.tagline = 'The square root of life is pi!';
-	$scope.title = "Geek Cities"
+	$scope.title = "Geek Cities";
+
+	$scope.submit - function() {
+		if ($scope.text) {
+
+		}
+	};
 });
