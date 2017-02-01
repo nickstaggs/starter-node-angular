d7791260e9d6cc186764ba78922ba7c7b22bc5ec
Nick Staggs <nick.c.staggs@gmail.com>
2017-02-01 18:12:03 -0500

    misc

--- app/routes.js
+++ app/routes.js
@@ -1,4 +1,4 @@
-var Nerd = require('./models/Nerd');
+var Nerd = require('./models/Nerd.js');
 
 
 module.exports = function(app) {
@@ -8,16 +8,29 @@ module.exports = function(app) {
 	// authentication routes
 	app.get('/api/nerds', function(req, res) {
             // use mongoose to get all nerds in the database
-      Nerd.find(function(err, nerds) {
-
-          // if there is an error retrieving, send the error.
-                          // nothing after res.send(err) will execute
-          if (err)
-              res.send(err);
-
-          res.json(nerds); // return all nerds in JSON format
-      });
+      // Nerd.find(function(err, nerds) {
+			//
+      //     // if there is an error retrieving, send the error.
+      //                     // nothing after res.send(err) will execute
+      //     if (err)
+      //         res.send(err);
+			//
+      //     res.json(nerds); // return all nerds in JSON format
+      // });
+			res.json("Momma we made it... to the db");
   });
+
+// API POST
+app.post('/api/nerds', function(req, res) {
+	Nerd.create({name: 'bob'}, function(err, body) {
+		if(err) {
+			console.log(err);
+			res.send(err);
+		}
+		return req;
+	});
+});
+
 	// frontend routes =========================================================
 	// route to handle all angular requests
 	app.get('*', function(req, res) {
--- config/db.js
+++ config/db.js
@@ -1,3 +1,3 @@
 module.exports = {
-	url : 'mongodb://localhost:27017/portfolioDB'
+	url : 'mongodb://localhost:27017/portfolioD'
 }
