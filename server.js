db4e9c415b696a26e1681a87e825d8ee3337c833
Nick Staggs <nick.c.staggs@gmail.com>
2017-02-01 18:09:24 -0500

    Attempting to add api

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var app            = express();
 var mongoose       = require('mongoose');
 var bodyParser     = require('body-parser');
 var methodOverride = require('method-override');
+var morgan = require('morgan');
 
 // configuration ===========================================
 
@@ -17,13 +18,14 @@ mongoose.connect(db.url); // connect to our mongoDB database (commented out afte
 app.use(bodyParser.json()); // parse application/json
 app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
 app.use(bodyParser.urlencoded({ extended: true })); // parse application/x-www-form-urlencoded
-
-app.use(methodOverride('X-HTTP-Method-Override')); // override with the X-HTTP-Method-Override header in the request. simulate DELETE/PUT
+app.use(morgan('dev'));
+app.use(methodOverride()); // override with the X-HTTP-Method-Override header in the request. simulate DELETE/PUT
 app.use(express.static(__dirname + '/public')); // set the static files location /public/img will be /img for users
 
-app.get('/', function(req, res) {
-	res.json({message: 'hooray, you accessed the database'});
-});
+
+// app.get('/', function(req, res) {
+// 	res.json('hooray, you accessed the database');
+// });
 
 // routes ==================================================
 require('./app/routes')(app); // pass our application into our routes
